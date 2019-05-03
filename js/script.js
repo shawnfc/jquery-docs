3cfcc9af3783a1c08c6b28ef85ce5c1656c5fef8
Shawn Conrad <shawnconrad@Shawns-MacBook-Pro.local>
2019-05-03 08:26:16 -0400

    dom manipulation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
-    var destination = $('a').attr('href', 'http://www.yahoo.com');
-    if($('div').length) {
-        console.log("Yes, that does exist.");
-        $('div').css({'width': '300px', 'height': '300px', 'background': 'blue'})
-    } else {
-        console.log("Nope!")
-    }
+    $('h1#firstH1').insertAfter($('h1').eq(1));
+
+    $('.container div#second').css({'background-color': 'blue', 'color': 'white'}).html('<h1>This is my div</h1>').width('300px').height('300px');
 });
